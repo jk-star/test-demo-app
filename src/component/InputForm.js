b98d9551ad2642de592d063f4f567280670c7e56
Jyoti Singh <113577693+jk-star@users.noreply.github.com>
2026-08-15 21:15:22 +0530

    level one task is done

--- src/component/InputForm.js
+++ src/component/InputForm.js
@@ -4,10 +4,23 @@ import React, { useRef } from 'react';
 
 const InputForm = () => {
     const refName = useRef(null);
-    const submitHandler = (event) =>{
+    const changeName = useRef(null);
+
+    const submitHandler = (event) => {
         event.preventDefault();
         console.log(refName.current.value);
     }
+
+    const inputFocus = (event) => {
+        event.preventDefault();
+        refName.current.focus();
+    }
+
+    const changeNameHandler = (event) => {
+        event.preventDefault();
+        changeName.current.value = "Jonh";
+    }
+
     return (
         <div className="container">
             <div className='row'>
@@ -17,15 +30,32 @@ const InputForm = () => {
                             <Form.Label>Name</Form.Label>
 
                             <Form.Control
+                                className='mb-3'
                                 ref={refName}
                                 type="text"
                                 placeholder="Enter your name"
                                 defaultValue=""
                             />
+
+                            <Form.Control
+                                ref={changeName}
+                                type="text"
+                                placeholder="Enter your name"
+                                defaultValue="Tom"
+                            />
+
                         </Form.Group>
 
-                        <Button variant="primary" type="submit">
+                        <Button variant="primary" type="submit" className='mx-3'>
                             Submit
+                        </Button>
+
+                        <Button variant="success" type="submit" onClick={inputFocus} className='mx-3'>
+                            Input Focus
+                        </Button>
+
+                        <Button variant="info" type="submit" onClick={changeNameHandler}>
+                            Change Name
                         </Button>
                     </Form>
                 </div>
